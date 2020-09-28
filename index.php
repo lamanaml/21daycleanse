1f6c8b46a4ed4bc1baddf3efd76a37ae9a7cf861
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-09-28 15:57:37 -0400

    updated cards

--- index.php
+++ index.php
@@ -67,20 +67,23 @@
           </p>  -->
         </div>
             
-            <div class="card-container">
-  <div class="card u-clearfix">
-    <div class="card-body">
-      <span class="card-number card-circle subtle">01</span>
-      <span class="card-author subtle">John Smith</span>
-      <h2 class="card-title">New Brunch Recipe</h2>
-      <span class="card-description subtle">These last few weeks I have been working hard on a new brunch recipe for you all.</span>
-      <div class="card-read">Read</div>
-      <span class="card-tag card-circle subtle">C</span>
+   <div class="card-container">
+      <div class="card u-clearfix">
+        <div class="card-body">
+          <span class="card-number card-circle subtle">01</span>
+          <span class="card-author subtle">John Smith</span>
+          <h2 class="card-title">New Brunch Recipe</h2>
+          <span class="card-description subtle">These last few weeks I have been working hard on a new brunch recipe for you all.</span>
+          <div class="card-read">Read</div>
+          <span class="card-tag card-circle subtle">C</span>
+        </div>
+        <img src="https://s15.postimg.cc/temvv7u4r/recipe.jpg" alt="" class="card-media" />
+      </div>
+      <div class="card-shadow"></div>
     </div>
-    <img src="https://s15.postimg.cc/temvv7u4r/recipe.jpg" alt="" class="card-media" />
-  </div>
-  <div class="card-shadow"></div>
-</div>
+
+
+
 <!-- Cards -->
         <div class="container" style="margin-top:20px; background-color: white;">
       <div class="card-group ">
