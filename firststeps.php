07b4eb544775cd3a2d4761b5846e6cc5b3ee313b
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-09-29 13:38:06 -0400

    updated cards

--- firststeps.php
+++ firststeps.php
@@ -44,23 +44,18 @@
    
 <!-- Hero -->
     
-    <div class="jumbotron" style="margin:0px; background-color: white;">
+    <div class="jumbotron" >
       <img src="assets/images/pagetitles/firststeps.png" width="100%"  >   
-    
-      
     </div>
     
     
-
-
-
-<!-- Cards -->
-   <div class="container" style="margin-top:20px; background-color: white;">
-   <p class="lead"> Let's take a few days to arm ourselves with the knowledge needed to crush this cleanse. Below are the different phases of the cleanse. We explain each phase, what it means, what to eat, what not to eat, and give you a few activities to help to make sure you understand.  This First Steps section is here to help make sure we have all of the information that we need before we start on day 1.  Let's have fun with the learning and activities, as the more time we spend here, the more prepared we will be for our journey! </p>
+   <div class="container" >
+   <p class="card-text"> Let's take a few days to arm ourselves with the knowledge needed to crush this cleanse. Below are the different phases of the cleanse. We explain each phase, what it means, what to eat, what not to eat, and give you a few activities to help to make sure you understand.  This First Steps section is here to help make sure we have all of the information that we need before we start on day 1.  Let's have fun with the learning and activities, as the more time we spend here, the more prepared we will be for our journey! </p>
       <div>
         <h1 class="display-4">The Phases</h1>
       </div>
-      
+
+      <!-- Cards -->
       <div class="card-group ">
       
         <div class="card shadow p-3 mb-5 bg-white rounded">
