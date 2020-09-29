042653c5293340a85bc79c48500de96598e4709e
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-09-29 18:10:05 -0400

    updated nav bar

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;1,400;1,500;1,700&display=swap" rel="stylesheet">
 
     <!-- <link rel="stylesheet" href="http://www.culinaryhealing.com/assets/css/lamana.css"> -->
-    <link rel="stylesheet" href="assets/css/21day.css">
+    <link rel="stylesheet" href="http://www.lamanadonadelle.com/21daycleanse/assets/css/21day.css">
   </head>
 
 
