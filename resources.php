448938b69ceec685b970e35ef57c7c96aa5b137e
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-10-01 17:40:38 -0400

    updated roadmap

--- resources.php
+++ resources.php
@@ -70,11 +70,13 @@
 
     </div>
 
-
+   <div>
+      <?php include_once("includes/faq.html"); ?> 
+     </div>
     <!-- Footer-->
     <div>
       <?php include_once("includes/footer.html"); ?> 
-     /div>
+     </div>
 
       <!-- Scripts -->
       <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
