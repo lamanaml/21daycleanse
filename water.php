b5cb7292820c844b6e17d7ffc66fd60eab8c3dab
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-10-02 18:07:11 -0400

    updated roadmap

---
+++ water.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+  <head>
+    <link rel="icon"  type="image/jpg" href="assets/images/favicon.jpg">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>21-Day Cleanse - Water - Culinary Healing</title>
+   <meta name="robots" content="noimageindex, noindex, nofollow, nosnippet">
+<!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-26808506-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'UA-26808506-1');
+    </script>
+
+    
+    
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">    
+
+    
+    <link rel="stylesheet" href="assets/css/21day.css">
+  </head>
+
+
+  <body id="liquids">
+      
+<!-- navigation -->
+    <div class="nav-div">
+      <?php include_once("includes/nav.html"); ?>
+    </div>
+     
+   
+<!-- Header -->
+      <div class="container">
+        <h1 >Water</h1>
+  <!-- content Body -->
+        <div class="container intro">  
+          <h3 >The Water Phase</h3>
+          <p class="card-text">During Water Day, we only drink distilled water.  No tea, no juice, no lemon in the water, no supplements.  Just distilled water at room temperature.  
+          </p> 
+          <p class="card-text">Drink at least 8 cups of water throughout the day to stay hydrated. Distilled water is best for the water day because it actually goes in and pulls out toxins from the organs. It literally pulls out the mire that gets caught in the follicles of the colon that breeds disease.</p> 
+          <p class="card-text">The most important thing for this day is to rest.  Go out to a park and just relax, stay in bed all day, meditate, pray, relax on the beach, do some yoga. I forbid you to do anything strenuous this day...I mean it! </p> 
+       
+          <h3>Activities</h3>
+          <ul>
+            <li>Rest</li>
+            <li>Sleep</li>
+            <li>Write in your journal</li>
+            <li>Yoga</li>
+            <li>Pray/meditate</li>
+          </ul>
+        </div>
+      </div>
+
+  
+   <!-- Footer-->
+    <div>
+      <?php include_once("includes/footer.html"); ?> 
+    </div>
+
+
+    
+
+
+      <!-- Scripts -->
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+      <script type="text/javascript" src="http://www.culinaryhealing.com/assets/javascript/lamana.js"></script>
+    </body>
+</html>
