18619d73df5ae6468c21d74cbaedf2407c99469a
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-10-02 09:20:08 -0400

    updated faqs

--- resources.php
+++ resources.php
@@ -64,7 +64,7 @@
 
       <div class="media">
         <div class="media-body">
-        <h5 class="mt-0"><a href="https://calendar.google.com/calendar/b/2?cid=Y3VsaW5hcnloZWFsaW5nQGdtYWlsLmNvbQ">>> Calendar</a></h5>
+        <h5 class="mt-0"><a href="https://calendar.google.com/calendar/b/2?cid=Y3VsaW5hcnloZWFsaW5nQGdtYWlsLmNvbQ">>> Sync with your Calendar</a></h5>
         </div>
       </div>
 
