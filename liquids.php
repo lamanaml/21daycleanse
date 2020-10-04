e2f81aa937518898c2078df68a4e0fadd0f57381
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-10-03 21:46:53 -0400

    updated notes link

--- liquids.php
+++ liquids.php
@@ -56,16 +56,14 @@
       <h3 class="card-title">Tips</h3>
       <h5 class="card-title">Cleaning  fruits and veggies</h5>
       <p class="card-text"> Fill your sink with cold water, add 4 tablespoons of salt and the juice of a lemon. Soak the fruits and veggies for 10 minutes then rinse under cold water. 1/4 cup of vinegar can be substituted for lemon.</p>
+
+       <div >
+        <?php include_once("includes/juicers.html"); ?>
+      </div>
     
       <h5 class="card-title">Storing your Juice</h5>
       <p class="card-text">We drink fresh fruit and veggie juice because it still has all its nutrients and enzymes. This means that the juice still has it's "life force". This life force is what will give you energy throughout the cleanse. Fresh juice begins to lose its nutrients immediately, therefore it's best to drink fresh juice right away. </p>
     
-
- <div >
-        <?php include_once("includes/juicers.html"); ?>
-      </div>
-
-
       <p class="card-text">
       The tips that follow will save you time in the kitchen by showing you how to store juices and keep as much of the life force intact as possible.
       </p>
