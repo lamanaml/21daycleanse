c590502e928ac7018279e62b22bb0aa7cf910633
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-10-07 01:42:15 -0400

    updated css

--- live.php
+++ live.php
@@ -52,21 +52,20 @@
     <h3 >What are Live (also know as Raw) foods?</h3>
     <p class="card-text">Live / raw foods are whole foods that have not been heated over a certain temperature, so they still have the vitamins, minerals, phytochemicals, and specifically the enzymes intact. 
     </p>
-    <p class="card-text" >
     <h5>What are Enzymes?</h5>
     <ul class="default">
       <li>Enzymes are the life force that conducts and directs every activity in our body.</li>
       <li>Enzymes "digest" or break down food in our digestive systems.</li>
       <li>Eating an enzyme-rich diet is thought to increase vitality, slows the aging process, help repair our DNA and RNA, breaks down proteins, increase weight loss and eliminate toxins.</li>
     </ul>
-    </p>
+  
     <p class="card-text">Enzymes are heat sensitive and destroyed at temperatures above 115 degrees. This is important because the purpose of eating is to provide our bodies with these nutrients and enzymes, and if they are destroyed through cooking, we are not giving our bodies what it needs... which defeats the point... right? 
     </p>
       <p class="card-text">The terms Raw and Live are sometimes used interchangeably, however there is a slight difference.  Technically speaking, raw food is in a dormant state where the enzymes have not yet been activated.  So, for example, sunflower seeds are raw.  You can keep it indefinitely  and it will continue to be a seed.  However, if you soak the seed overnight (removing the enzyme inhibitors), it will start to grow a sprout.  Soaking it changes the state from raw to live, meaning the enzymes have become active and it can now produce life. Therefore, Live food is always Raw, but not all Raw food is Live.   In general, our diets should consist of at least 50% live foods. Eating live can mean something as simple as eating an apple or a salad, or can be so sophisticated that you will forget that it is raw! 
     </p>
     <p class="card-text">So, during this phase (days 3-7 and days 15-19), we will turn off the stoves and flood ourselves with nutrients and life giving enzymes!</p>
   
-        <p class="card-text">Need a little inspiration?  Check out some images of Live/Faw Food meals from folks over the years!</p>
+    <p class="card-text">Need a little inspiration?  Check out some images of Live/Faw Food meals from folks over the years!</p>
     </div>      
         <!-- Carousel -->
         <div class="container">
