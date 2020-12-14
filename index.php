cc3bc2f94db32c8213eb655ce0a471aefc4b01dd
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-12-14 12:23:27 -0500

    added signin

--- index.php
+++ index.php
@@ -13,47 +13,26 @@
 
       </head>  
       <body>  
-        <?php  
+       <?php
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+        header('Access-Control-Allow-Headers: token, Content-Type');
+        header('Access-Control-Max-Age: 1728000');
+        header('Content-Length: 0');
+        header('Content-Type: text/plain');
+        die();
+    }
+
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+
+    $ret = [
+        'result' => 'OK',
+    ];
+    print json_encode($ret);
     
 
- // Access-Control headers are received during OPTIONS requests
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");         
-
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-
-} ?>
-  
-           <br />  
-           <div class="container" style="width:700px;">  
-                <h3 style="align:center">Culinary Healing Sign-in</h3><br />  
-                <br />  
-                <br />  
-                <br />  
-                <br />  
-                <br />  
-                <?php  
-                if(isset($_SESSION['username']))  
-                {  
-                ?>  
-                <div style="align:center">  
-                     <h1>Welcome - <?php echo $_SESSION['username']; ?></h1><br />  
-                     <a href="#" id="logout">Logout</a>  
-                </div>  
-                <?php  
-                }  
-                else  
-                {  
-                ?>  
-                <div style="align:center">  
-                     <button type="button" name="login" id="login" class="btn btn-success" data-toggle="modal" data-target="#loginModal">Login</button>  
-                </div>  
-                <?php  
-                }  
-                ?>  
            </div>  
            <br />  
       </body>  
