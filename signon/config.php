c6d12a366c096e0d113d5b2cda86cd3cd535f52f
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-12-16 18:02:35 -0500

    updated cards

--- signon/config.php
+++ signon/config.php
@@ -3,7 +3,7 @@
 server with default setting (user 'root' with no password) */
  define('DB_SERVER', 'localhost');
    define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', 'T@b00@rI');
+   define('DB_PASSWORD', 'T@b00@r!');
    define('DB_NAME', 'jlodywmy_21day');
  
 /* Attempt to connect to MySQL database */
