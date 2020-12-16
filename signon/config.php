1e1565b760af249d90594d84db445fb31c123d02
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-12-16 17:53:32 -0500

    updated cards

--- signon/config.php
+++ signon/config.php
@@ -1,7 +1,16 @@
 <?php
-   define('DB_SERVER', 'localhost');
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+ define('DB_SERVER', 'localhost:3306');
    define('DB_USERNAME', 'root');
    define('DB_PASSWORD', '1234');
-   define('DB_DATABASE', 'jlodywmy_21day');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+   define('DB_NAME', '21day');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
 ?>
