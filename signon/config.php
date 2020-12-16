586b3ffd0d509cdc39b6347731e3006e6a9843d3
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-12-15 23:20:12 -0500

    updated cards

--- signon/config.php
+++ signon/config.php
@@ -1,7 +1,7 @@
 <?php
-   define('DB_SERVER', 'jlodywmy_21day');
+   define('DB_SERVER', 'localhost');
    define('DB_USERNAME', 'root');
    define('DB_PASSWORD', '1234');
-   define('DB_DATABASE', '21day');
+   define('DB_DATABASE', 'jlodywmy_21day');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
