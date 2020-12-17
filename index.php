3e61da2f134c0c44cd1d516fa95888bde98bf6b4
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-12-16 22:09:34 -0500

    updated sql

--- index.php
+++ index.php
@@ -106,8 +106,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                   
                     <h2>Culinary Healing 21-Day Cleanse</h2>
                 
-                <img src="_authenticated/assets/images/chlogo-tran.png">
-                    
+                <img src="_authenticated/assets/images/chlogo-tran.png" width="300px">
+
                     <p>Please add your username and password to access the 21-Day Cleanse</p>
                     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                         <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
