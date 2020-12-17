32327a4b45b5cc4d71351ed2e1335625bf32c829
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-12-16 19:03:42 -0500

    updated sql

--- signon/config.php
+++ signon/config.php
@@ -2,7 +2,7 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
   define('DB_SERVER', 'localhost:3306');
-   define('DB_USERNAME', 'jlodywmy_lamanaml');
+   define('DB_USERNAME', 'jlodywmy_lamana');
    define('DB_PASSWORD', 'T@b00@r!');
    define('DB_NAME', 'jlodywmy');
  
