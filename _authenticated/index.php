9cfa2adb8890bf374d7df8d8583dfd7c12db2351
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-12-16 20:10:06 -0500

    updated sql

--- _authenticated/index.php
+++ _authenticated/index.php
@@ -32,7 +32,13 @@
 
 
   <body id="welcome">
-    
+      <div class="page-header">
+        <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
+    </div>
+    <p>
+        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
+        <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
+    </p>
 
 <!-- navigation -->
     <div class="nav-div">
