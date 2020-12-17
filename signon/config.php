4331eb832dfa10dfb908a19cd85f51370e34b0b5
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-12-16 19:08:55 -0500

    updated sql

--- signon/config.php
+++ signon/config.php
@@ -2,9 +2,9 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
   define('DB_SERVER', 'localhost:3306');
-   define('DB_USERNAME', 'jlodywmy_lamana');
-   define('DB_PASSWORD', 'T@b00@r!');
-   define('DB_NAME', 'jlodywmy_21day');
+   define('DB_USERNAME', 'jewels');
+   define('DB_PASSWORD', 'T@b000@r!');
+   define('DB_NAME', 'jlodywmy_jewels_outreach);
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
