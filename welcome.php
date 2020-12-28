a488632f95ecffb8d98082e8e7f0b7e66fe03a5a
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-12-28 18:30:41 -0500

    updated cards

--- welcome.php
+++ welcome.php
@@ -1,6 +1,9 @@
 <?php
 // Initialize the session
 session_start();
+
+// Include config file
+require_once "config.php";
  
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
