4617fa18b7056143801a760a112013c2a8d00d1b
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-12-28 18:13:22 -0500

    updated cards

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ session_start();
  
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: _authenticated/index.php");
+    header("location: welcome.php");
     exit;
 }
  
