035722563973174cc94df72d02f68343179355d1
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-12-28 19:52:06 -0500

    updated cards

--- index.php
+++ index.php
@@ -114,7 +114,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">    
 
     
-    <link rel="stylesheet" href="assets/css/21day.css">
+    <link rel="stylesheet" href="_authenticated/assets/css/21day.css">
   </head>
 
 <body>
