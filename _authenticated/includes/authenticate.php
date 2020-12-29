af95e0330f7bbd2a7eabe2c84727f069de1da810
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-12-28 19:23:03 -0500

    updated cards

--- _authenticated/includes/authenticate.php
+++ _authenticated/includes/authenticate.php
@@ -1,6 +1,6 @@
-  <body id="welcome">
-  <div class="page-header">
-        <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
+
+  <div >
+        <h3>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h3>
     </div>
     <p>
         <a href="../reset-password.php" class="btn btn-warning">Reset Your Password</a>
