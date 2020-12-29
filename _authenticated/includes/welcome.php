2539d3725ca94094ff7461ea2cfb1d699b3e1be7
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-12-28 19:08:53 -0500

    updated cards

---
+++ _authenticated/includes/welcome.php
@@ -0,0 +1,25 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: ../index.php");
+    exit;
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+    <div class="page-header">
+        <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
+    </div>
+    <p>
+        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
+        <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
+    </p>
+</body>
+</html>
+If data comes from external sources lik
