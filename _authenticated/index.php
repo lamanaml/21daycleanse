78f98718a0d23ee6156ae42c9b22e4a19e71fa14
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-12-28 19:16:31 -0500

    updated cards

--- _authenticated/index.php
+++ _authenticated/index.php
@@ -1,4 +1,13 @@
-
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: index.php");
+    exit;
+}
+?>
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
@@ -33,7 +42,13 @@
 
    
   <body id="welcome">
- 
+  <div class="page-header">
+        <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
+    </div>
+    <p>
+        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
+        <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
+    </p>
 
 <!-- navigation -->
     <div class="nav-div">
