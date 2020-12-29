ca7b94e51c0f79560ba8cdb7a163dcb0bcc8c3d5
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-12-28 19:25:07 -0500

    updated cards

--- _authenticated/includes/authenticate.php
+++ _authenticated/includes/authenticate.php
@@ -3,5 +3,5 @@
         <h3>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?>
     </div>
     <p>
-        <a href="../reset-password.php" >Reset Your Password</a> <a href="../logout.php" >Sign Out of Your Account</a>
+        <a href="../reset-password.php" >Reset Your Password</a> <a href="../logout.php" >Sign Out </a>
     </p>
