ff519fe18358a75c1a33451e3887a9daa37fc128
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-12-28 19:11:46 -0500

    updated cards

--- _authenticated/includes/authenticate.php
+++ _authenticated/includes/authenticate.php
@@ -9,10 +9,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 }
 ?>
  
-<!DOCTYPE html>
-<html lang="en">
 
-<body>
     <div class="page-header">
         <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
     </div>
@@ -20,5 +17,3 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
         <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
         <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
     </p>
-</body>
-</html>
